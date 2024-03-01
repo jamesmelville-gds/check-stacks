eee72de257583937cd3247f3bf977146a1e53e1a
James Melville <james.melville@digital.cabinet-office.gov.uk>
2024-03-01 21:03:16 +0000

    Add describe_security_group_rules

--- ec2.py
+++ ec2.py
@@ -9,3 +9,12 @@ def describe_network_interfaces(session: Session):
     for page in paginator:
         for interface in page["NetworkInterfaces"]:
             yield interface
+
+def describe_security_group_rules(session: Session, SecurityGroupRuleIds: list):
+    ec2 = session.client("ec2")
+
+    paginator = ec2.get_paginator("describe_security_group_rules").paginate(SecurityGroupRuleIds=SecurityGroupRuleIds)
+
+    for page in paginator:
+        for rule in page["SecurityGroupRules"]:
+            yield rule
